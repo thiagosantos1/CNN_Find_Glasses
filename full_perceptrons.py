c1e96eb1bc1fee2593529e9eb22639244ecebbd6
Thiago Santos <thiagogyn1@hotmail.com>
2018-10-09 21:43:52 -0400

    new readme

--- full_perceptrons.py
+++ full_perceptrons.py
@@ -1,5 +1,11 @@
+"""
+Author: Thiago Santos
+Master thesis project - Fall 2018
+
+The idea is to test our model with only 1 layer(full layer of perceptrons), without hidden or convolution layers. 
+Our goal is to get a better result than if a computer were just guessing.
+"""
+
 #!/usr/bin/env python3
 # chmod u+x
 # -*- coding: utf-8 -*-  
-
-
