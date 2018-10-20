769aa77a71ef39edcf2faf5a8db08442f98e594a
Thiago Santos <tsantos2@sycamores.indstate.edu>
2018-10-19 23:41:21 -0400

    New Conv

--- cnn_net.py
+++ cnn_net.py
@@ -67,7 +67,7 @@ def next_batch(MINIBATCH_SIZE, X_train, y_train, size_train):
 def main(): 
 
   #### Data pre-processing
-  X_all, y_all = load_img_lbl_idx3(dataset='all', path='dataset', rotate=False) 
+  X_all, y_all = load_img_lbl_idx3(dataset='all', path='dataset', rotate=True) 
   X_train, X_test, y_train, y_test = train_test_split(X_all, y_all, test_size=0.2, shuffle=True)
 
   size_train = len(X_train)
